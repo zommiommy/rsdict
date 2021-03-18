f6509b0f93fa3a688eb4f121ef21804aa54995ca
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2021-03-18 19:21:17 +0100

    simplified the hash

--- src/hash.rs
+++ src/hash.rs
@@ -3,40 +3,6 @@ use std::hash::{Hash, Hasher};
 
 impl Hash for RsDict {
     fn hash<H: Hasher>(&self, state: &mut H) {
-        self.len.hash(state);
-        self.num_ones.hash(state);
-        self.num_zeros.hash(state);
-
-        self.sb_classes.hash(state);
-        self.sb_indices.hash(state);
-
-        self.large_blocks.hash(state);
-
-        self.select_one_inds.hash(state);
-        self.select_zero_inds.hash(state);
-        
-        self.last_block.hash(state);
-    }
-}
-
-impl Hash for LastBlock {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.bits.hash(state);
-        self.num_ones.hash(state);
-        self.num_zeros.hash(state);
-    }
-}
-
-impl Hash for LargeBlock {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.pointer.hash(state);
-        self.rank.hash(state);
-    }
-}
-
-impl Hash for VarintBuffer {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.buf.hash(state);
-        self.len.hash(state);
+        self.iter().for_each(|x| x.hash(state));
     }
 }
