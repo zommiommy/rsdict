b4c13d46c41e5ddac7f589006caba728849f3edd
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2021-03-18 22:56:29 +0100

    WIP iter_in_range

--- src/iter.rs
+++ src/iter.rs
@@ -11,6 +11,13 @@ impl<'a> IntoIterator for &'a RsDict {
 }
 
 impl<'a> RsDict {
+    /// Return an iterator over all the indices of the bits set to one
+    /// which are inside the provided range.
+    pub fn iter_in_range(&'a self, range: Range<u64>) -> Option<RsDictIterator<'a>> {
+        RsDictIterator::new_in_range(self, range)
+    }
+}
+impl<'a> RsDict {
     /// return an Iterator over the indices of the bits set to one in the RsDict.
     pub fn iter(&'a self) -> RsDictIterator<'a> {
         self.into_iter()
@@ -23,9 +30,11 @@ pub struct RsDictIterator<'a> {
     ptr: usize,
     index: usize,
     max_index: usize,
+    max: Option<u64>,
 }
 
 impl<'a> RsDictIterator<'a> {
+    /// Create a structure that iter over all the indices of the bits set to one.
     pub fn new(father: &RsDict) -> RsDictIterator {
         let class = father.sb_classes[0];
         let code_length = ENUM_CODE_LENGTH[class as usize] as usize;
@@ -37,7 +46,76 @@ impl<'a> RsDictIterator<'a> {
             ptr: 0,
             index: 0,
             max_index: father.sb_classes.len(),
+            max: None,
         }
+    }
+
+    /// Create a structure that iter over all the indices of the bits set to one
+    /// which are inside the provided range.
+    pub fn new_in_range(father: &RsDict, range: Range<u64>) -> Option<RsDictIterator> {
+        let pos = range.start;
+
+        // if the start value is bigger than all the rest, return an empty iterator
+        if pos >= father.len() as u64 {
+            return None;
+        }
+
+        let prefix_num_ones = father.num_ones - father.last_block.num_ones;
+        // if the start bit is in the last block, clear the code accordingly
+        if pos >= father.last_block_ind() {
+            // Get the current code
+            let mut code = father.last_block.bits;
+            // Clear the bits
+            code = clear_lower_bits(code, pos - father.last_block_ind());
+            // Return the iterator
+            return Some(
+                RsDictIterator{
+                    father:father,
+                    current_code: code,
+                    ptr: 0, // no need to initialize, it will never be used
+                    index: father.last_block_ind() as usize,
+                    max_index: father.sb_classes.len(),
+                    max: Some(range.end),
+                }
+            );
+        }
+
+        // Start with the rank from our position's large block.
+        let lblock = pos / LARGE_BLOCK_SIZE;
+        let LargeBlock {
+            mut pointer,
+            mut rank,
+        } = father.large_blocks[lblock as usize];
+
+        // Add in the ranks (i.e. the classes) per small block up to our
+        // position's small block.
+        let sblock_start = (lblock * SMALL_BLOCK_PER_LARGE_BLOCK) as usize;
+        let sblock = (pos / SMALL_BLOCK_SIZE) as usize;
+        // Scan the small blocks from the start of the large block
+        // to the current small block to compute the pointer in the enumerative
+        // codes array.
+        let (class_sum, length_sum) =
+            rank_acceleration::scan_block(&father.sb_classes, sblock_start, sblock);
+        // Update the pointer and the rank with the sum of the values,
+        rank += class_sum;
+        pointer += length_sum;
+        // Get the class of the current block
+        let sb_class = father.sb_classes[sblock];
+        // Read the code
+        let mut  code = father.read_sb_index(pointer, ENUM_CODE_LENGTH[sb_class as usize]);
+        // filter the lower bits
+        code = clear_lower_bits(code, pos - rank);
+        // Create the iterator
+        Some(
+            RsDictIterator{
+                father:father,
+                current_code: code,
+                ptr: pointer as usize, // no need to initialize, it will never be used
+                index: sblock,
+                max_index: father.sb_classes.len(),
+                max: Some(range.end),
+            }
+        )
     }
 }
 
@@ -77,10 +155,39 @@ impl<'a> Iterator for RsDictIterator<'a> {
         let t = self.current_code.trailing_zeros();
         
         // clear it from the current code
-        self.current_code ^= 1 << t;
+        self.current_code = clear_lowest_bit_set(self.current_code);
+
         // compute the result value
         let result = self.index as u64 * SMALL_BLOCK_SIZE + t as u64;
 
+        // Check if we exceeds the max value
+        if let Some(_max) = &self.max {
+            if result >= *_max {
+                return None;
+            }
+        }
+
         Some(result)
     }
+}
+
+#[inline(always)]
+/// Clear the lowest set bit.
+fn clear_lowest_bit_set(x: u64) -> u64 {
+    // if possible, use the fast instruction
+    #[cfg(target_feature="bmi1")]
+    {
+        return unsafe{core::arch::x86_64::_blsr_u64(x)};
+    }
+    // Otherwise fall down to the generic implementation
+    #[cfg(not(target_feature="bmi1"))]
+    {
+        x & (x - 1)
+    }
+}
+
+#[inline(always)]
+/// Clear the lowest num bits.
+fn clear_lower_bits(code: u64, num:u64) -> u64 {
+    code & u64::MAX.wrapping_shl(num as u32)
 }
--- tests/test_iter.rs
+++ tests/test_iter.rs
@@ -1,6 +1,5 @@
 // other library used for correctness checks
 extern crate fid;
-use fid::{FID, BitVector};
 
 fn xorshift(mut x: u64) -> u64 {
     x ^= x << 13;
@@ -29,4 +28,53 @@ fn test_iter() {
     let indices: Vec<u64> = vector.iter().enumerate().filter(|(i, x)| **x).map(|(i, x)| i as u64).collect();
 
     assert_eq!(indices, r.iter().collect::<Vec<u64>>());;
+}
+
+#[test]
+/// Check that elias fano runs considering a lot of possible combinations.
+fn test_iter_in_range() {
+    let mut r = rsdict::RsDict::new();
+    let mut seed = 0xdeadbeef;
+    let size = 100usize;
+
+    for _ in 0..100 {
+        let mut vector = Vec::with_capacity(size);
+        let mut values = Vec::with_capacity(size);
+
+        for i in 0..size {
+            seed = xorshift(seed);
+            let val = (seed & 1) == 1;
+            vector.push(val);
+            r.push(val);
+            if val {
+                values.push(i);
+            }
+        }
+
+        seed = xorshift(seed);
+        let start = seed % (size + 10) as u64;
+        let mut end = start;
+        while end <= start {
+            seed = xorshift(seed);
+            end = seed % (size + 10) as u64;
+        }
+
+        let indices: Vec<usize> = values.iter()
+            .filter(|x| (start..end).contains(&(**x as u64)))
+            .cloned().collect();
+
+        let result: Vec<u64> = r.iter_in_range(start as u64..end as u64)
+            .unwrap().collect();
+
+        println!("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+        println!("Start {:10} End {:10}", start, end);
+        println!("Values: {:?}", values);
+        println!("Truth:  {:?}", indices);
+        println!("Result: {:?}", result);
+
+        assert_eq!(indices.len(), result.len());
+        for (a, b) in indices.iter().zip(result.iter()) {
+            assert_eq!(*a, *b as usize);
+        }
+    }
 }
